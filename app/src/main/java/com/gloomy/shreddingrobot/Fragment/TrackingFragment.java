0712c61ea6b5ea87d8e61e30cd3c167a13bb050e
Rufus Zhu <rufus-zhu@hotmail.com>
2015-02-04 23:30:36 -0800

    round image view

--- app/src/main/java/com/gloomy/shreddingrobot/Fragment/TrackingFragment.java
+++ app/src/main/java/com/gloomy/shreddingrobot/Fragment/TrackingFragment.java
@@ -1,16 +1,19 @@
 package com.gloomy.shreddingrobot.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gloomy.shreddingrobot.R;
+import com.gloomy.shreddingrobot.ResultActivity;
 import com.gloomy.shreddingrobot.Utility.BaseFragment;
 
 
 public class TrackingFragment extends BaseFragment {
-
+private Button btn;
     public static TrackingFragment newInstance() {
         return new TrackingFragment();
     }
@@ -18,6 +21,29 @@ public class TrackingFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_tracking, container, false);
     }
+    @Override
+    public void onResume(){
+        findView();
+        bindEvent();
+        super.onResume();
+    }
+
+    private void bindEvent() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void findView() {
+        btn = (Button) getView().findViewById(R.id.button);
+    }
+
+
 }
