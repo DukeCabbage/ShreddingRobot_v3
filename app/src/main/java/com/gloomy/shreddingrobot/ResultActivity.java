37812ee65cc0dcc75ea711554dbd3810ec7c356e
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2015-02-10 16:25:28 -0800

    re-write tracking page, add start btn with animation

--- app/src/main/java/com/gloomy/shreddingrobot/ResultActivity.java
+++ app/src/main/java/com/gloomy/shreddingrobot/ResultActivity.java
@@ -22,27 +22,9 @@ public class ResultActivity extends ActionBarActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             getWindow().setStatusBarColor(getResources().getColor(R.color.primary));
 
-        final TextView morph = (TextView) findViewById(R.id.tv_morph);
-        morph.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e(TAG, "onclick");
-                animateDrawables(morph);
-            }
-        });
 
     }
 
 
-    private void animateDrawables(View view) {
-        if (!(view instanceof TextView)) {
-            return;
-        }
-        TextView textView = (TextView) view;
 
-        if (textView.getBackground() instanceof Animatable) {
-            ((Animatable) textView.getBackground()).start();
-        }
-
-    }
 }
--- app/src/main/java/com/gloomy/shreddingrobot/UIFragment/TrackingFragment.java
+++ app/src/main/java/com/gloomy/shreddingrobot/UIFragment/TrackingFragment.java
@@ -1,11 +1,15 @@
 package com.gloomy.shreddingrobot.UIFragment;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.gloomy.shreddingrobot.R;
@@ -30,7 +34,25 @@ public class TrackingFragment extends BaseFragment
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracking, container, false);
         initView(rootView);
+        bindEvent(rootView);
         return rootView;
+    }
+
+    private void bindEvent(final View rootView) {
+        FrameLayout button = (FrameLayout) rootView.findViewById(R.id.btn_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), ResultActivity.class);
+//                startActivity(intent);
+                TransitionDrawable transition = (TransitionDrawable) v.getBackground();
+                transition.startTransition(1000);
+
+                TextView morph = (TextView) rootView.findViewById(R.id.tv_morph);
+                animateDrawables(morph);
+
+            }
+        });
     }
 
     private void initView(View rootView) {
@@ -39,15 +61,7 @@ public class TrackingFragment extends BaseFragment
 
     @Override
     public void onResume(){
-        Button button = (Button) getView().findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               Intent intent = new Intent(getActivity(), ResultActivity.class);
-               startActivity(intent);
 
-            }
-        });
         super.onResume();
 
     }
@@ -63,6 +77,18 @@ public class TrackingFragment extends BaseFragment
 
     @Override
     public void updateAltitude(double altitude) {
+
+    }
+
+    private void animateDrawables(View view) {
+        if (!(view instanceof TextView)) {
+            return;
+        }
+        TextView textView = (TextView) view;
+
+        if (textView.getBackground() instanceof Animatable) {
+            ((Animatable) textView.getBackground()).start();
+        }
 
     }
 }
